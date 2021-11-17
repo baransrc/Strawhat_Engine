e65d90dcc5a055f2fa88801bc9e885d1c1a5e11b
Baran Surucu <baransrc@gmail.com>
2021-11-17 05:53:22 +0100

    Added texture support to the shaders.

--- Game/fragment.glsl
+++ Game/fragment.glsl
@@ -1,7 +1,13 @@
 #version 330 core
 out vec4 frag_color;
 
+in vec3 interpolated_color;
+in vec2 interpolated_texture_coordinate;
+
+uniform sampler2D input_texture;
+
 void main()
 {
-    frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    // frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    frag_color = texture(input_texture, interpolated_texture_coordinate);
 } 
--- Game/vertex.glsl
+++ Game/vertex.glsl
@@ -1,7 +1,14 @@
 #version 330 core
 layout (location = 0) in vec3 vertex_position;
+layout (location = 1) in vec3 vertex_color;
+layout (location = 2) in vec2 vertex_texture_coordinate;
+
+out vec3 interpolated_color;
+out vec2 interpolated_texture_coordinate;
 
 void main()
 {
     gl_Position = vec4(vertex_position, 1.0);
+    interpolated_color = vertex_color;
+    interpolated_texture_coordinate = vertex_texture_coordinate;
 }
