66b242372d85a32fffdf4a85eaa076e24cd6b531
Baran Surucu <baransrc@gmail.com>
2021-12-16 05:46:45 +0100

    Added initial Entity method and member declarations.

--- Source/Entity.h
+++ Source/Entity.h
@@ -1,5 +1,32 @@
 #pragma once
+
+#include <vector>
+#include <string>
+#include "ComponentType.h"
+
+class Component;
+
 class Entity
 {
-};
+private:
+	std::vector<Component*> components;
+	std::vector<Entity*> children;
+	Entity* parent;
+	std::string name;
+	int id;
+	bool active;
 
+public:
+	Entity();
+	~Entity();
+
+	void Update();
+
+	void AddComponent(Component* component);
+	void AddComponent(component_type type);
+	Component* CreateComponent(component_type type) const;
+	Component* GetComponent(component_type type);
+
+	const Entity* GetParent() const;
+	void AddChild(const Entity* child);
+};
