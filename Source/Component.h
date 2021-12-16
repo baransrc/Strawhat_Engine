f7d19bc85ad3385a1e1d2a2e83c6e32deb1753e4
Baran Surucu <baransrc@gmail.com>
2021-12-16 05:46:37 +0100

    Added initial Component method and member declarations.

--- Source/Component.h
+++ Source/Component.h
@@ -1,5 +1,22 @@
 #pragma once
+
+#include "ComponentType.h"
+
+class Entity;
+
 class Component
 {
-};
+protected:
+	bool enabled;
+	Entity* owner;
+public:
+	Component();
+	virtual ~Component() = default;
 
+	virtual component_type Type();
+	virtual void Initialize();
+	virtual void Enable();
+	virtual void Disable();
+	virtual void Update();
+	virtual bool IsEnabled();
+};
