ffe458d0aaf30f46c649502596072db3bc0ba62f
Baran Surucu <baransrc@gmail.com>
2021-12-19 17:53:02 +0100

    Added to string functionality to component type.

--- Source/ComponentType.h
+++ Source/ComponentType.h
@@ -7,3 +7,30 @@ enum class component_type
 	CAMERA,
 	MESH,
 };
+
+inline const char* component_type_to_string(component_type type)
+{
+	switch (type)
+	{
+	case component_type::UNDEFINED:
+	{
+		return "Undefined Component";
+	}
+	case component_type::TRANSFORM:
+	{
+		return "Transform Component";
+	}
+	case component_type::CAMERA:
+	{
+		return "Camera Component";
+	}
+	case component_type::MESH:
+	{
+		return "Mesh Component";
+	}
+	default:
+	{
+		return "Refer to ComponentType.h and add this component to the function.";
+	}
+	}
+}
