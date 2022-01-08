22fa275a9f18c9134c08bf165631002bb35dd8ca
Baran Surucu <baransrc@gmail.com>
2022-01-08 17:17:01 +0100

    Added method function support to Event, wrapped function ptrs with EventListener.

--- Source/Event.h
+++ Source/Event.h
@@ -1,7 +1,36 @@
 #pragma once
 
 #include <vector>
+#include <functional>
+
 #include "Globals.h"
+
+template<typename... ARGS> class EventListener
+{
+private:
+	std::function<void(ARGS... args)> callback;
+
+public:
+	EventListener();
+	EventListener(std::function<void(ARGS... args)> new_callback);
+
+public:
+	void operator() (ARGS... args)
+	{
+		callback(args...);
+	}
+};
+
+template<typename... ARGS>
+inline EventListener<ARGS...>::EventListener()
+{
+}
+
+template<typename... ARGS>
+inline EventListener<ARGS...>::EventListener(std::function<void(ARGS... args)> new_callback)
+{
+	callback = new_callback;
+}
 
 // NOTE: Right now this Event template class only accepts functions with 
 // any type/number of parameters but only with return type of void.
@@ -10,15 +39,17 @@
 template<typename... ARGS> class Event
 {
 private:
-	std::vector<void(*)(ARGS... args)> listeners;
+	std::vector<EventListener<ARGS...>*> listeners;
+
 public:
-	void AddListener(void(*listener)(ARGS... args));
-	void RemoveListener(void(*listener)(ARGS... args));
+	void AddListener(EventListener<ARGS...>* listener);
+	void RemoveListener(EventListener<ARGS...>* listener);
+	void RemoveAllListeners();
 	void Invoke(ARGS... args);
 };
 
 template<typename... ARGS>
-void Event<ARGS...>::AddListener(void(*listener)(ARGS... args))
+inline void Event<ARGS...>::AddListener(EventListener<ARGS...>* listener)
 {
 	LOG("Added Listener");
 
@@ -26,11 +57,15 @@ void Event<ARGS...>::AddListener(void(*listener)(ARGS... args))
 }
 
 template<typename... ARGS>
-void Event<ARGS...>::RemoveListener(void(*listener)(ARGS... args))
+inline void Event<ARGS...>::RemoveListener(EventListener<ARGS...>* listener)
 {
 	LOG("Removed Listener");
 
-	std::vector<void(*)(ARGS... args)>::template iterator index_to_erase;
+	std::vector<EventListener<ARGS...>*>::template iterator index_to_erase;
+
+	// NOTE: Right now this determines index to erase by comparing the memory addresses,
+	// making each instance of EventListener hold a unique ID would be a better solution.
+	// TODO: Address this issue.
 
 	index_to_erase = std::find(listeners.begin(), listeners.end(), listener);
 
@@ -41,12 +76,22 @@ void Event<ARGS...>::RemoveListener(void(*listener)(ARGS... args))
 }
 
 template<typename... ARGS>
-void Event<ARGS...>::Invoke(ARGS... args)
+inline void Event<ARGS...>::RemoveAllListeners()
+{
+	listeners.clear();
+
+	// NOTE: If we decide to add a reference to the registered Event on EventListeners, 
+	// this RemoveAllListener method might call a function that deregisters the listener 
+	// from event.
+}
+
+template<typename... ARGS>
+inline void Event<ARGS...>::Invoke(ARGS... args)
 {
 	LOG("Invoked event");
 
-	for (void(*listener)(ARGS... args) : listeners)
+	for (EventListener<ARGS...>* listener : listeners)
 	{
-		listener(args...);
+		(*listener)(args...);
 	}
 }
