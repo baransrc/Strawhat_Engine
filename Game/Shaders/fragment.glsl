a52b2ee41a0faa9695b2dc6e68a89fdba768ec17
Monica <monicamartin13@hotmail.com>
2022-01-18 02:03:15 +0100

    Added to shaders Ambient Color and Directional Light test working

--- Game/Shaders/fragment.glsl
+++ Game/Shaders/fragment.glsl
@@ -6,8 +6,30 @@ in vec2 interpolated_texture_coordinate;
 
 uniform sampler2D input_texture;
 
+uniform vec3 light_direction;
+uniform vec3 diffuse_color;
+uniform vec3 ambient_color;
+
+in vec3 world_normal;
+
 void main()
 {
-    // frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-    frag_color = texture(input_texture, interpolated_texture_coordinate);
+    //frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    //frag_color = texture(input_texture, interpolated_texture_coordinate);
+    vec3 light_direction = vec3 (0.5,0,0.5);
+    vec3 ambient_color = vec3 (0.2,0.2,0.2);
+    vec3 diffuse_color = texture2D(input_texture, interpolated_texture_coordinate).rgb;
+    vec3 N  = normalize(world_normal);
+    vec3 L = normalize(light_direction);
+
+    float NdotL = -dot(N,L);
+
+    if(NdotL >= 0.0)
+    {
+        frag_color = vec4(diffuse_color*NdotL, 1.0);
+    } 
+    else
+    {
+        frag_color = vec4(ambient_color*diffuse_color, 1.0);
+    }
 } 
--- Game/Shaders/vertex.glsl
+++ Game/Shaders/vertex.glsl
@@ -9,9 +9,11 @@ uniform mat4 model_matrix;
 
 out vec3 interpolated_color;
 out vec2 interpolated_texture_coordinate;
+out vec3 world_normal;
 
 void main()
 {
+    world_normal = transpose(inverse(mat3(projection_matrix)))*vertex_color;
     gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertex_position, 1.0);
     interpolated_color = vertex_color;
     interpolated_texture_coordinate = vertex_texture_coordinate;
