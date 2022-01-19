2dbd406ab9cea86e4c11f233050c13c55ab6100c
Monica <monicamartin13@hotmail.com>
2022-01-19 04:27:06 +0100

    Added Directional Light functionality

--- Source/ComponentLight.cpp
+++ Source/ComponentLight.cpp
@@ -1,1 +1,109 @@
 #include "ComponentLight.h"
+
+#include "Application.h"
+#include "ModuleShaderProgram.h"
+
+#include "GLEW/include/GL/glew.h"
+
+ComponentLight::ComponentLight() : Component(),
+												type(light_type::DIRECTIONAL),
+												position(0,0,0),
+												//rotation(0,0,0,0),
+												scale(1,1,1),
+												color(1,1,1),
+												is_currently_loaded(false)
+
+{
+}
+
+ComponentLight::~ComponentLight()
+{
+	Reset();
+}
+
+component_type ComponentLight::Type() const
+{
+	return component_type::LIGHT;
+}
+
+void ComponentLight::Initialize(Entity* new_owner)
+{
+	Component::Initialize(new_owner);
+}
+
+void ComponentLight::Load(light_type new_type, float3 new_position, /*Quat new_rotation,*/ float3 new_scale, float3 new_color)
+{
+	type = new_type;
+	position = new_position;
+	//rotation = new_rotation;
+	scale = new_scale;
+	color = new_color;
+
+	// If Load was called before this call, clear 
+	// all the previous mesh data and load afterwards:
+	//if (is_currently_loaded)
+	//{
+	//	Reset();
+	//}
+
+
+	switch (type)
+	{
+	case light_type::POINT:
+	{
+		break;
+	}
+	case light_type::DIRECTIONAL:
+	{
+		// Make sure we are using the true shader before passing the arguments:
+		App->shader_program->Use();
+
+		// Pass position of the directional light
+		App->shader_program->SetUniformVariable("directional_light", position);
+		App->shader_program->SetUniformVariable("directional_color", color);
+
+		break;
+	}
+	case light_type::SPOT:
+	{
+		break;
+	}
+	default:
+	{
+		LOG("The light type is crashing bro");
+		break;
+	}
+	}
+
+
+	// Set as currently loaded:
+	is_currently_loaded = true;
+}
+
+void ComponentLight::Update()
+{
+	App->shader_program->SetUniformVariable("directional_light", position);
+	App->shader_program->SetUniformVariable("directional_color", color);
+}
+
+void ComponentLight::Reset()
+{
+	is_currently_loaded = false;
+
+	/*free(indices);
+	free(vertices);
+	free(texture_ids);
+
+	indices = nullptr;
+	vertices = nullptr;*/
+
+}
+
+void ComponentLight::DrawGizmo()
+{
+	Component::DrawGizmo();
+}
+
+void ComponentLight::DrawInspectorContent()
+{
+}
--- Source/ComponentLight.h
+++ Source/ComponentLight.h
@@ -1,6 +1,54 @@
 #pragma once
 #include "Component.h"
+#include "MATH_GEO_LIB/Math/float3.h"
+#include "MATH_GEO_LIB/Math/Quat.h"
+
+
+enum class light_type
+{
+	SPOT,
+	DIRECTIONAL,
+	POINT,
+};
+
 class ComponentLight : public Component
 {
+private:
+
+	light_type type;
+
+	float3 position;
+	//Quat rotation;
+	float3 scale;
+
+	float3 color;
+
+	bool is_currently_loaded;
+
+public:
+	ComponentLight();
+	~ComponentLight() override;
+
+	component_type Type() const override;
+	void Initialize(Entity* new_owner) override;
+	void Load(
+		light_type new_type, 
+		float3 new_position, 
+		//Quat new_rotation, 
+		float3 new_scale, 
+		float3 new_color
+	);
+	void Update() override;
+	void Reset();
+	void DrawGizmo() override;
+
+	light_type GetLightType() const { return type; };
+	float3 GetLightPosition() const { return position; };
+	//Quat GetLightRotation() const { return rotation; };
+	float3 GetLightScale() const { return scale; };
+	float3 GetLightColor() const { return color; };
+
+protected:
+	void DrawInspectorContent() override;
 };
 
