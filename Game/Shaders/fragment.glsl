c4cff50912dfff3832483789309e962d9661e593
Monica <monicamartin13@hotmail.com>
2022-01-19 04:26:10 +0100

    Changed uniform that now receives info from Component Light

--- Game/Shaders/fragment.glsl
+++ Game/Shaders/fragment.glsl
@@ -6,7 +6,8 @@ in vec2 interpolated_texture_coordinate;
 
 uniform sampler2D input_texture;
 
-uniform vec3 light_direction;
+uniform vec3 directional_light;
+uniform vec3 directional_color;
 uniform vec3 diffuse_color;
 uniform vec3 ambient_color;
 
@@ -16,11 +17,10 @@ void main()
 {
     //frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
     //frag_color = texture(input_texture, interpolated_texture_coordinate);
-    vec3 light_direction = vec3 (0.5,0,0.5);
     vec3 ambient_color = vec3 (0.2,0.2,0.2);
     vec3 diffuse_color = texture2D(input_texture, interpolated_texture_coordinate).rgb;
     vec3 N  = normalize(world_normal);
-    vec3 L = normalize(light_direction);
+    vec3 L = normalize(directional_light);
 
     float NdotL = -dot(N,L);
 
