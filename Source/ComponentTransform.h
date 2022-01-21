ca0a96004aa38f4f036cf60d95e2bad71699e12e
Baran Surucu <baransrc@gmail.com>
2022-01-21 02:55:03 +0100

    Created ComponentTransform class and added method declarations.

---
+++ Source/ComponentTransform.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "MATH_GEO_LIB/Math/float3.h"
+#include "MATH_GEO_LIB/Math/Quat.h"
+#include "MATH_GEO_LIB/Math/float4x4.h"
+
+#include "Component.h"
+class ComponentTransform : public Component
+{
+private:
+	math::float3 position;
+	math::float3 position_local;
+	math::float3 scale;
+	math::float3 scale_local;
+	math::float3 rotation_euler;
+	math::float3 rotation_euler_local;
+	math::Quat rotation;
+	math::Quat rotation_local;
+	math::float4x4 matrix;
+	math::float4x4 matrix_local;
+	math::float3 right;
+	math::float3 up;
+	math::float3 front;
+
+	bool should_calculate_matrix;
+	bool should_calculate_local_matrix;
+
+public:
+	ComponentTransform();
+	~ComponentTransform() override;
+
+	void Initialize(Entity* new_owner) override;
+	void Update() override;
+	void DrawGizmo() override;
+
+	component_type Type() const override;
+
+	const math::float3& GetPosition() const;
+	const math::float3& GetScale() const;
+	const math::float3& GetEulerRotation() const;
+	const math::float3& GetLocalPosition() const;
+	const math::float3& GetLocalScale() const;
+	const math::float3& GetLocalEulerRotation() const;
+	const math::Quat& GetRotation() const;
+	const math::Quat& GetLocalRotation() const;
+	const math::float4x4& GetMatrix() const;
+	const math::float4x4& GetLocalMatrix() const;
+	const math::float3& GetRight() const;
+	const math::float3& GetUp() const;
+	const math::float3& GetFront() const;
+
+	void SetPosition(const math::float3& new_position);
+	void SetScale(const math::float3& new_scale);
+	void SetEulerRotation(const math::float3& new_rotation_euler);
+	void SetRotation(const math::Quat& new_rotation);
+	void SetLocalPosition(const math::float3& new_position_local);
+	void SetLocalScale(const math::float3& new_scale_local);
+	void SetLocalEulerRotation(const math::float3& new_rotation_euler_local);
+	void SetLocalRotation(const math::Quat& new_rotation_local);
+
+protected:
+	void DrawInspectorContent() override;
+
+private: 
+	static void CalculateMatrixFrom(const math::float3& new_position, 
+									const math::float3& new_rotation_euler, 
+									const math::float3& new_scale);
+	void CalculateMatrix();
+	void CalculateLocalMatrix();
+};
