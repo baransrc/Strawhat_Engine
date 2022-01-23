ccd52712d5a86631af9e8447d52fc2cde59cfbe5
Monica <monicamartin13@hotmail.com>
2022-01-23 05:18:40 +0100

    Working on directional light with PBR implementation using materials

--- Game/Shaders/fragment.glsl
+++ Game/Shaders/fragment.glsl
@@ -1,4 +1,7 @@
 #version 330 core
+
+#define PI 3.141597
+
 out vec4 frag_color;
 
 struct Light {
@@ -16,10 +19,21 @@ struct Light {
     float quadratic;
 }; 
 
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    sampler2D occlusion;
+    sampler2D normalmap;
+    vec4 color;
+};
+
 
 in vec3 interpolated_colorNORMALS;
 in vec2 interpolated_texture_coordinate;
 in vec3 fragment_position;
+
+
+uniform vec3 camera_position;
 
 uniform sampler2D input_texture;
 
@@ -28,11 +42,43 @@ uniform vec3 directional_color;
 uniform vec3 diffuse_color;
 uniform vec3 ambient_color;
 uniform Light light;
+uniform Material material;
 
 in vec3 world_normal;
 
+vec3 SchlickFresnel(const vec3 f0, float cos_theta)
+{
+    return f0 + (vec3(1.0) - f0) * pow(1.0 - cos_theta, 5.0);
+}
+
 void main()
 {
+    vec3 N  = normalize(world_normal);
+    vec3 L = normalize(light.direction);
+
+    float NdotL = -dot(N,L);
+
+    vec3 view_direction = normalize(camera_position - fragment_position);
+    float VdotR =  dot(view_direction, reflect(light.direction, world_normal));
+
+
+    vec3 fresnel = SchlickFresnel(texture(material.diffuse, interpolated_texture_coordinate).rgb, NdotL);
+    float shininess = 1.5;
+    float attenuation = 1.0;
+    vec3 step1 = (texture(material.diffuse, interpolated_texture_coordinate).rgb * (vec3(1.0) - texture(material.specular, interpolated_texture_coordinate).rgb)) / PI;
+    vec3 step2 = ((shininess + 2.0) / (2.0 * PI)) * fresnel * pow(VdotR, shininess);
+    vec3 PBR = light.diffuse * (step1 + step2) * NdotL * attenuation;
+
+    
+    if(NdotL >= 0.0)
+    {
+        frag_color = vec4(PBR, 1.0);
+    } 
+    else
+    {
+        frag_color = vec4(light.ambient*texture(material.diffuse, interpolated_texture_coordinate).rgb, 1.0);
+    }
+
     //frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
     //frag_color = texture(input_texture, interpolated_texture_coordinate);
    /* vec3 ambient_color = vec3 (0.2,0.2,0.2);
@@ -50,44 +96,46 @@ void main()
     {
         frag_color = vec4(ambient_color*diffuse_color, 1.0);
     }*/
-
-    vec3 lightDir = normalize(light.position - fragment_position);
+//
+//    vec3 lightDir = normalize(light.position - fragment_position);
+//    
+//    // check if lighting is inside the spotlight cone
+//    float theta = dot(lightDir, normalize(-light.direction)); 
     
-    // check if lighting is inside the spotlight cone
-    float theta = dot(lightDir, normalize(-light.direction)); 
-    
-    if(theta > light.cutOff) // remember that we're working with angles as cosines instead of degrees so a '>' is used.
-    {    
-        // ambient
-        vec3 ambient = light.ambient * texture(input_texture, interpolated_texture_coordinate).rgb;
-        
-        // diffuse 
-        vec3 norm = normalize(interpolated_colorNORMALS);
-        float diff = max(dot(norm, lightDir), 0.0);
-        vec3 diffuse = light.diffuse * diff * texture(input_texture, interpolated_texture_coordinate).rgb;  
-        
-        // specular
-        vec3 viewDir = normalize(light.position - fragment_position);
-        vec3 reflectDir = reflect(-lightDir, norm);  
-        float shininess = 32.0f;
-        float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
-        vec3 specular = light.specular * spec * texture(input_texture, interpolated_texture_coordinate).rgb;  
-        
-        // attenuation
-        float distance    = length(light.position - fragment_position);
-        float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
-
-        //ambient  *= attenuation; // remove attenuation from ambient, as otherwise at large distances the light would be darker inside than outside the spotlight due the ambient term in the else branche
-        //diffuse   *= attenuation;
-        //specular *= attenuation;   
-            
-        vec3 result = ambient + diffuse /*+ specular*/;
-        frag_color = vec4(result, 1.0);
-    }
-    else 
-    {
-        // else, use ambient light so scene isn't completely dark outside the spotlight.
-        frag_color = vec4(light.ambient * texture(input_texture, interpolated_texture_coordinate).rgb, 1.0);
-        //frag_color = vec4((0.2, 0.2, 0.2) * texture(input_texture, interpolated_texture_coordinate).rgb, 1.0);
-    }
+//    if(theta > light.cutOff) // remember that we're working with angles as cosines instead of degrees so a '>' is used.
+//    {    
+//        // ambient
+//        vec3 ambient = light.ambient * texture(material.diffuse, interpolated_texture_coordinate).rgb;
+//        
+//        // diffuse 
+//        vec3 norm = normalize(interpolated_colorNORMALS);
+//        float diff = max(dot(norm, lightDir), 0.0);
+//        vec3 diffuse = light.diffuse * diff * texture(material.diffuse, interpolated_texture_coordinate).rgb;  
+//        
+//        // specular
+//        vec3 viewDir = normalize(light.position - fragment_position);
+//        vec3 reflectDir = reflect(-lightDir, norm);  
+//        float shininess = 32.0f;
+//        float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+//        vec3 specular = light.specular * spec * texture(material.diffuse, interpolated_texture_coordinate).rgb;  
+//        
+//        // attenuation
+//        float distance    = length(light.position - fragment_position);
+//        float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+//
+//        //ambient  *= attenuation; // remove attenuation from ambient, as otherwise at large distances the light would be darker inside than outside the spotlight due the ambient term in the else branche
+//        //diffuse   *= attenuation;
+//        //specular *= attenuation;   
+//            
+//        vec3 result = ambient + diffuse /*+ specular*/;
+//        frag_color = vec4(result, 1.0);
+//    }
+//    else 
+//    {
+//        // else, use ambient light so scene isn't completely dark outside the spotlight.
+//        frag_color = vec4(light.ambient * texture(material.diffuse, interpolated_texture_coordinate).rgb, 1.0);
+//        //frag_color = vec4((0.2, 0.2, 0.2) * texture(input_texture, interpolated_texture_coordinate).rgb, 1.0);
+//    }
+    //frag_color = vec4(0.3, 1.0, 0.3, 1.0);
+    //frag_color = vec4(light.ambient * texture(material.diffuse, interpolated_texture_coordinate).rgb, 1.0);
 } 
