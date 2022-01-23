2dfca1c02b59f1c792ebcba693d63fc8fb6edc41
Monica <monicamartin13@hotmail.com>
2022-01-23 05:14:02 +0100

    Working on component material

--- Source/ComponentMaterial.cpp
+++ Source/ComponentMaterial.cpp
@@ -1,1 +1,122 @@
 #include "ComponentMaterial.h"
+
+#include "Application.h"
+#include "ModuleShaderProgram.h"
+#include "ModuleDebugDraw.h"
+
+#include "GLEW/include/GL/glew.h"
+
+#include "Entity.h"
+
+ComponentMaterial::ComponentMaterial() : Component(),
+										texture_ids(nullptr),
+										number_of_texture_ids(0),
+										color_diffuse(0.2f, 0.2f, 0.2f, 1.0f),
+										shininess(0),
+										is_currently_loaded(false)
+{
+}
+
+ComponentMaterial::~ComponentMaterial()
+{
+	Reset();
+}
+
+component_type ComponentMaterial::Type() const
+{
+	return component_type::MATERIAL;
+}
+
+void ComponentMaterial::Initialize(Entity* new_owner)
+{
+	Component::Initialize(new_owner);
+}
+
+void ComponentMaterial::Load(const unsigned int* new_texture_ids, size_t new_number_of_texture_ids)
+{
+	//number_of_texture_ids = new_number_of_texture_ids;
+	number_of_texture_ids = 3;
+
+	texture_ids = (unsigned int*)malloc(sizeof(unsigned int) * number_of_texture_ids);
+	for (size_t i = 0; i < number_of_texture_ids; ++i)
+	{
+		texture_ids[i] = new_texture_ids[i];
+		//LOG("Texture %d id %d", i, texture_ids[i]);
+	}
+
+	// Use the shader:
+	App->shader_program->Use();
+	// Activate Texture Unit 0:
+	glActiveTexture(GL_TEXTURE0);
+	// Bind Texture Unit 0:
+	glBindTexture(GL_TEXTURE_2D, texture_ids[0]); // Diffuse texture
+	// Set Texture Parameter in shader:
+	App->shader_program->SetUniformVariable("material.diffuse", 0);
+
+	// Activate Texture Unit 1:
+	glActiveTexture(GL_TEXTURE1);
+	// Bind Texture Unit 1:
+	glBindTexture(GL_TEXTURE_2D, texture_ids[1]); // Specular texture
+	// Set Texture Parameter in shader:
+	App->shader_program->SetUniformVariable("material.specular", 1);
+
+	// Activate Texture Unit 2:
+	glActiveTexture(GL_TEXTURE2);
+	// Bind Texture Unit 2:
+	glBindTexture(GL_TEXTURE_2D, texture_ids[2]); // Occlusion texture
+	// Set Texture Parameter in shader:
+	App->shader_program->SetUniformVariable("material.occlusion", 2);
+
+	// Activate Texture Unit 3:
+	glActiveTexture(GL_TEXTURE3);
+	// Bind Texture Unit 3:
+	glBindTexture(GL_TEXTURE_2D, texture_ids[3]); // NormalMap texture
+	// Set Texture Parameter in shader:
+	App->shader_program->SetUniformVariable("material.normal", 3);
+
+	// Set as currently loaded:
+	is_currently_loaded = true;
+
+	// Invoke change in parent and its ancestors:
+	owner->InvokeComponentsChangedEvents(Type());
+}
+
+void ComponentMaterial::Update()
+{
+}
+
+void ComponentMaterial::Use()
+{
+	// Use the shader :
+	App->shader_program->Use();
+	// Activate Texture Unit 0:
+	glActiveTexture(GL_TEXTURE0);
+	// Bind Texture Unit 0:
+	glBindTexture(GL_TEXTURE_2D, texture_ids[0]); // Diffuse texture
+	// Set Texture Parameter in shader:
+	App->shader_program->SetUniformVariable("material.diffuse", 0);
+
+	// Activate Texture Unit 1:
+	glActiveTexture(GL_TEXTURE1);
+	// Bind Texture Unit 1:
+	glBindTexture(GL_TEXTURE_2D, texture_ids[1]); // Specular texture
+	// Set Texture Parameter in shader:
+	App->shader_program->SetUniformVariable("material.specular", 1);
+
+	// Activate Texture Unit 2:
+	glActiveTexture(GL_TEXTURE2);
+	// Bind Texture Unit 2:
+	glBindTexture(GL_TEXTURE_2D, texture_ids[2]); // Occlusion texture
+	// Set Texture Parameter in shader:
+	App->shader_program->SetUniformVariable("material.occlusion", 2);
+}
+
+void ComponentMaterial::Reset()
+{
+	is_currently_loaded = false;
+}
+
+
+void ComponentMaterial::DrawInspectorContent()
+{
+}
--- Source/ComponentMaterial.h
+++ Source/ComponentMaterial.h
@@ -1,6 +1,41 @@
 #pragma once
 #include "Component.h"
+
+#include "MATH_GEO_LIB/Math/float4.h"
+
 class ComponentMaterial : public Component
 {
+private:
+	unsigned int* texture_ids;
+	//diffuse 0
+	//specular 1
+	//oclussion 2
+	//normalmap 3
+
+	size_t number_of_texture_ids;
+
+	float4 color_diffuse;
+
+	float shininess;
+
+	bool is_currently_loaded;
+
+public:
+	ComponentMaterial();
+	~ComponentMaterial() override;
+
+	component_type Type() const override;
+	void Initialize(Entity* new_owner) override;
+	void Load(
+		const unsigned int* new_texture_ids, 
+		size_t new_number_of_texture_ids
+	);
+	void Update() override;
+	void Use();
+	void Reset();
+
+protected:
+	void DrawInspectorContent() override;
+
 };
 
