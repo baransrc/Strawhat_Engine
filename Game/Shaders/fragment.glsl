9e3efdf1b6117f264878d6143f9973a779dffaf8
Monica <monicamartin13@hotmail.com>
2022-01-24 02:41:43 +0100

    Working on PRB

--- Game/Shaders/fragment.glsl
+++ Game/Shaders/fragment.glsl
@@ -31,6 +31,8 @@ struct Material {
 in vec3 interpolated_colorNORMALS;
 in vec2 interpolated_texture_coordinate;
 in vec3 fragment_position;
+in vec3 fragment_normal;
+in vec2 fragment_texture_coordinate;
 
 
 uniform vec3 camera_position;
@@ -39,12 +41,13 @@ uniform sampler2D input_texture;
 
 uniform vec3 directional_light;
 uniform vec3 directional_color;
-uniform vec3 diffuse_color;
+//uniform vec3 diffuse_color;
 uniform vec3 ambient_color;
 uniform Light light;
 uniform Material material;
 
 in vec3 world_normal;
+
 
 vec3 SchlickFresnel(const vec3 f0, float cos_theta)
 {
@@ -53,31 +56,35 @@ vec3 SchlickFresnel(const vec3 f0, float cos_theta)
 
 void main()
 {
-    vec3 N  = normalize(world_normal);
-    vec3 L = normalize(light.direction);
+    vec3 N  = normalize(fragment_normal);
+    vec3 L = normalize(-light.direction);
 
-    float NdotL = -dot(N,L);
+    float NdotL = max(dot(N,L), 0.0001);
 
     vec3 view_direction = normalize(camera_position - fragment_position);
-    float VdotR =  dot(view_direction, reflect(light.direction, world_normal));
+    float VdotR =  max(dot(view_direction, reflect(-L, fragment_normal)), 0.0001);
 
+    vec3 diffuse_color = texture(material.diffuse, fragment_texture_coordinate).rgb;
 
-    vec3 fresnel = SchlickFresnel(texture(material.diffuse, interpolated_texture_coordinate).rgb, NdotL);
-    float shininess = 1.5;
+    vec3 fresnel = SchlickFresnel(texture(material.specular, fragment_texture_coordinate).rgb, NdotL);
+    float shininess = 25.0;
     float attenuation = 1.0;
-    vec3 step1 = (texture(material.diffuse, interpolated_texture_coordinate).rgb * (vec3(1.0) - texture(material.specular, interpolated_texture_coordinate).rgb)) / PI;
+    vec3 step1 = (diffuse_color * (vec3(1.0) - texture(material.specular, fragment_texture_coordinate).rgb)) / PI;
     vec3 step2 = ((shininess + 2.0) / (2.0 * PI)) * fresnel * pow(VdotR, shininess);
     vec3 PBR = light.diffuse * (step1 + step2) * NdotL * attenuation;
 
-    
-    if(NdotL >= 0.0)
-    {
-        frag_color = vec4(PBR, 1.0);
-    } 
-    else
-    {
-        frag_color = vec4(light.ambient*texture(material.diffuse, interpolated_texture_coordinate).rgb, 1.0);
-    }
+    PBR += light.ambient*diffuse_color;
+
+    frag_color = vec4(PBR, 1.0);
+      //frag_color = vec4(texture(material.specular, fragment_texture_coordinate).rgb, texture(material.specular, fragment_texture_coordinate).a);
+//    if(NdotL >= 0.0)
+//    {
+//        frag_color = vec4(PBR, 1.0);
+//    } 
+//    else
+//    {
+//        frag_color = vec4(light.ambient*texture(material.diffuse, fragment_texture_coordinate).rgb, 1.0);
+//    }
 
     //frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
     //frag_color = texture(input_texture, interpolated_texture_coordinate);
