71be2dd72acf2067463d4d5e70c66178673c335b
Baran Surucu <baransrc@gmail.com>
2022-01-30 03:45:14 +0100

    Added Hierarchy Drag & Drop functionality.

--- Source/ModuleSceneManager.cpp
+++ Source/ModuleSceneManager.cpp
@@ -124,6 +124,8 @@ void ModuleSceneManager::DrawRecursiveEntityHierarchy(Entity* entity, bool is_ro
 	}
 
 	bool being_renamed = renamed_entity_in_hierarchy != nullptr && renamed_entity_in_hierarchy->Id() == entity->Id();
+	// This is updated again according to whether the node is collapsed or 
+	// expanded:
 	bool open = false;
 	// should_be_deleted is set to true if delete was selected
 	// and it gets deleted after the tree is rendered:
@@ -131,31 +133,38 @@ void ModuleSceneManager::DrawRecursiveEntityHierarchy(Entity* entity, bool is_ro
 	// should_move_up_in_hierarchy is set to true if the current entity "Move Up"
 	// is called:
 	bool should_move_up_in_hierarchy = false;
+	// Dropped child will be accessed like this at the end of the function if it's set to a non-nullptr entity
+	Entity* new_dropped_child_entity = nullptr;
 
 	// Get a unique id of the current node for click events and etc to be unique to
 	// that node:
 	char main_id_buffer[64];
 	sprintf(main_id_buffer, "ent##%u\0", entity->Id());
 
-	ImGui::PushID(main_id_buffer);
-
+	// Set the flags of the node:
 	ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_FramePadding;
-
+	// If there are no child entities don't draw the arrow and
+	// disable being collapsable/expandable:
 	if (entity->GetChildren().size() <= 0)
 	{
 		flags |= ImGuiTreeNodeFlags_Leaf;
 	}
 
+	// Cache selected entity in the current scene so we don't have to
+	// call the getter everytime:
 	Entity* selected_entity_in_hierarchy = current_scene->GetSelectedEntity();
-
 	bool is_selected = selected_entity_in_hierarchy != nullptr && selected_entity_in_hierarchy->Id() == entity->Id();
 
+	// If the entity is selected, render it as selected node:
 	if (is_selected)
 	{
 		flags |= ImGuiTreeNodeFlags_Selected;
 	}
 
+	// Render the tree node of entity, return if it's expanded or not:
 	open = ImGui::TreeNodeEx(being_renamed ? "" : entity->Name().c_str(), flags);
+
+	ImGui::PushID(main_id_buffer);
 
 	// If Current node is clicked:
 	if (ImGui::IsItemClicked(ImGuiMouseButton_Left))
@@ -165,12 +174,69 @@ void ModuleSceneManager::DrawRecursiveEntityHierarchy(Entity* entity, bool is_ro
 			renamed_entity_in_hierarchy = nullptr;
 		}
 
+		// Update is selected as the item is clicked:
+		is_selected = true;
+		// Update the cached one as well:
 		selected_entity_in_hierarchy = entity;
+		// Update selected entity in the scene hierarchy:
 		current_scene->SetSelectedEntity(selected_entity_in_hierarchy);
 	}
 
-	is_selected = selected_entity_in_hierarchy != nullptr && selected_entity_in_hierarchy->Id() == entity->Id();
-		
+	// Set this as a drag and drop target:
+	if (ImGui::BeginDragDropTarget())
+	{
+		// Accept payloads of the type "entity_in_hierarchy":
+		ImGui::AcceptDragDropPayload("entity_in_hierarchy");
+
+		// Get the current drag drop payload ImGui has:
+		const ImGuiPayload* drag_drop_payload = ImGui::GetDragDropPayload();
+
+		// Check if it's valid for us to process:
+		const bool is_entity = drag_drop_payload->IsDataType("entity_in_hierarchy");
+		const bool is_delivery = drag_drop_payload->IsDelivery();
+
+		if (is_delivery && is_entity)
+		{
+			// Get the payload data as unsigned int*:
+			unsigned int* drag_dropped_entity_id = ((unsigned int*)drag_drop_payload->Data);
+
+			// Received ptr to id should not be null and Corresponding Entity 
+			// to the received id should not be the same with target's id:
+			if (drag_dropped_entity_id != nullptr && *drag_dropped_entity_id != entity->Id())	
+			{
+				// Find the entity in the scene as all the entities are descendants of the root node of
+				// the scene:
+				Entity* dropped_entity = current_scene->GetRootEntity()->FindDescendant(*drag_dropped_entity_id);
+				
+				// Avoid dragged entity to be dropped on an entity that is 
+				// an ancestor of it:
+				const bool is_dropped_entity_not_an_ancestor = 
+					dropped_entity != nullptr &&
+					!dropped_entity->HasDescendant(entity->Id());
+				// Set the new_dropped_child_entity to this dropped_entity, 
+				// this will be used at the end of the function, that is not
+				// a recursive area, and gets added as a child to entity:
+				new_dropped_child_entity = is_dropped_entity_not_an_ancestor ? dropped_entity : nullptr;
+			}
+		}
+
+		ImGui::EndDragDropTarget();
+	}
+
+	// If this is not the root entity, set the node as 
+	// drag and droppable in the hierarchy:
+	if (!is_root_entity && ImGui::BeginDragDropSource())
+	{
+		// This was purely made bc ImGui wants const void*:
+		unsigned int data[1] = { entity->Id() };
+
+		// Set the drag and drop payload data and type, data is
+		// the id of this dragged entity:
+		ImGui::SetDragDropPayload("entity_in_hierarchy", data, 1);
+
+		ImGui::EndDragDropSource();
+	}
+
 	// Rename text input area:
 	if (being_renamed)
 	{
@@ -187,7 +253,6 @@ void ModuleSceneManager::DrawRecursiveEntityHierarchy(Entity* entity, bool is_ro
 		ImGui::AlignTextToFramePadding();
 
 		ImGui::PushStyleColor(ImGuiCol_FrameBg, ImVec4(1.0f, 1.0f, 1.0f, 0.5f));
-		//ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
 		ImGui::PushID(rename_id_buffer);
 
 		if (ImGui::InputTextWithHint("", entity->Name().c_str(), &buffer[0], 1024, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_AlwaysOverwrite))
@@ -197,7 +262,6 @@ void ModuleSceneManager::DrawRecursiveEntityHierarchy(Entity* entity, bool is_ro
 		}
 
 		ImGui::PopID();
-		//ImGui::PopStyleVar();
 		ImGui::PopStyleColor();
 	}
 
@@ -215,6 +279,8 @@ void ModuleSceneManager::DrawRecursiveEntityHierarchy(Entity* entity, bool is_ro
 			child->SetParent(entity);
 		}
 
+		// If the entity is the root entity of the scene
+		// don't show rename, delete and move up options:
 		if (!is_root_entity)
 		{
 			const bool dont_show_move_up = entity->Id() == current_scene->GetRootEntity()->Id() || 
@@ -250,6 +316,7 @@ void ModuleSceneManager::DrawRecursiveEntityHierarchy(Entity* entity, bool is_ro
 		ImGui::EndPopup();
 	}
 
+	// If expanded, draw all the children:
 	if (open)
 	{
 		if (entity->GetChildren().size() > 0)
@@ -264,6 +331,13 @@ void ModuleSceneManager::DrawRecursiveEntityHierarchy(Entity* entity, bool is_ro
 
 	ImGui::PopID();	
 
+	// This means we have a new entity drag drop on our entity:
+	if (new_dropped_child_entity != nullptr)
+	{
+		new_dropped_child_entity->SetParent(entity);
+	}
+
+	// This means move up option was selected on this entity:
 	if (should_move_up_in_hierarchy)
 	{
 		Entity* previous_parent = entity->Parent();
@@ -276,6 +350,7 @@ void ModuleSceneManager::DrawRecursiveEntityHierarchy(Entity* entity, bool is_ro
 		}
 	}
 
+	// This means delete option was selected on this entity:
 	if (should_be_deleted)
 	{
 		entity->SetParent(nullptr);
