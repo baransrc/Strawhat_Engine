e542c6c523b60db5d63f434efe583d2163036635
Baran Surucu <baransrc@gmail.com>
2022-02-02 02:34:25 +0100

    Fixed memory leak bc of forgotten commented line.

--- Source/ModelImporter.cpp
+++ Source/ModelImporter.cpp
@@ -379,7 +379,7 @@ namespace ModelImporter
 		// Deallocate resources:
 		free(model_name);
 		//free(texture_ids);
-		//free(path_to_parent_directory);
+		free(path_to_parent_directory);
 		//free(name_of_file);
 
 		return loaded_model;
