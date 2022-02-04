a69a0e82b4e8cceba4ae4b9c96e244b7d84dccab
Baran Surucu <baransrc@gmail.com>
2022-02-04 03:55:45 +0100

    Fixed bugs, cleaned ComponentBoundingBox and made it aware of hierarchy changes.

--- Source/ComponentBoundingBox.cpp
+++ Source/ComponentBoundingBox.cpp
@@ -2,8 +2,6 @@
 #include "ComponentMesh.h"
 #include "ComponentTransform.h"
 
-#include "Entity.h"
-
 #include "Application.h"
 #include "ModuleDebugDraw.h"
 
@@ -20,8 +18,11 @@ ComponentBoundingBox::~ComponentBoundingBox()
 {
     if (owner != nullptr)
     {
+        // Unsubscribe from the component changed events of the owner:
         owner->GetComponentsChangedEvent()->RemoveListener(&component_changed_event_listener);
         owner->GetComponentsChangedInDescendantsEvent()->RemoveListener(&component_changed_event_listener);
+        // Unsubscribe from the hierarchy changed event of the owner:
+        owner->GetHierarchyChangedEvent()->RemoveListener(&hierarchy_changed_event_listener);
     }
 }
 
@@ -35,9 +36,17 @@ void ComponentBoundingBox::Initialize(Entity* new_owner)
     Component::Initialize(new_owner);
 
     Load();
+
+    // Create hierarchy changed event listener:
+    hierarchy_changed_event_listener = EventListener<entity_operation>(std::bind(&ComponentBoundingBox::HandleHierarchyChanged, this, std::placeholders::_1));
+    // Subscribe it to the owner's get hierarchy changed event:
+    owner->GetHierarchyChangedEvent()->AddListener(&hierarchy_changed_event_listener);
     
+    // Create component changed event listener:
     component_changed_event_listener = EventListener<component_type>(std::bind(&ComponentBoundingBox::HandleComponentChanged, this, std::placeholders::_1));
-    
+    // Subscribe it to the components changed event and
+    // the components changed in descendants event of the
+    // owner:
     owner->GetComponentsChangedEvent()->AddListener(&component_changed_event_listener);
     owner->GetComponentsChangedInDescendantsEvent()->AddListener(&component_changed_event_listener);
 }
@@ -60,69 +69,57 @@ void ComponentBoundingBox::Load()
     math::AABB aabb;
     aabb.SetNegativeInfinity();
 
-    std::vector<ComponentMesh*> mesh_components = owner->GetComponentsInChildren<ComponentMesh>();
-
-    // TODO(Baran): Refactor this code to remove duplicate code here.
+    std::vector<ComponentMesh*> mesh_components = owner->GetComponentsInDescendants<ComponentMesh>();
 
     ComponentMesh* owner_mesh_component = owner->GetComponent<ComponentMesh>();
-    if (owner_mesh_component != nullptr && mesh_components.size() > 0)
-    {
-        mesh_components.push_back(owner_mesh_component);
 
+    if (mesh_components.size() > 0)
+    {
+        // If the owner also has a mesh component, add it to the list:
+        if (owner_mesh_component != nullptr)
+        {
+            mesh_components.push_back(owner_mesh_component);
+        }
+
+        // Calculate the aabb from the list:
         for (ComponentMesh* mesh_component : mesh_components)
         {
-            const math::AABB& mesh_aabb = mesh_component->GetAABB();
-            math::OBB local_obb;
-
-            local_obb.SetFrom(mesh_aabb);
-            local_obb.Scale(math::float3::zero, mesh_component->Owner()->Transform()->GetScale());
-            local_obb.Transform(mesh_component->Owner()->Transform()->GetRotation());
-            local_obb.Translate(mesh_component->Owner()->Transform()->GetPosition());
-
-            aabb.Enclose(local_obb);
+            AddMeshComponentToAABB(mesh_component, aabb);
         }
-    }
-    else if (owner->GetComponent<ComponentMesh>() != nullptr && mesh_components.size() <= 0)
-    {
-        const math::AABB& mesh_aabb = owner_mesh_component->GetAABB();
-        math::OBB local_obb;
 
+        // Set the obb from aabb:
+        obb.SetFrom(aabb);
+    }
+    else if (owner_mesh_component != nullptr && mesh_components.size() <= 0)
+    {
+        // NOTE: This is not added like the previous if statement 
+        // because here we directly get the aabb and set an obb
+        // from it and transforming it.
+
+        // Get aabb of owner mesh component:
+        const math::AABB& mesh_aabb = owner_mesh_component->GetAABB();
+
+        // Set the obb from owner mesh component's aabb:
         obb.SetFrom(mesh_aabb);
+        // Transform it in the order of Scale->Rotate->Translate:
         obb.Scale(math::float3::zero, Owner()->Transform()->GetScale());
         obb.Transform(Owner()->Transform()->GetRotation());
         obb.Translate(Owner()->Transform()->GetPosition());
     }
-    else if (owner->GetComponent<ComponentMesh>() == nullptr && mesh_components.size() > 0)
-    {
-        for (ComponentMesh* mesh_component : mesh_components)
-        {
-            const math::AABB& mesh_aabb = mesh_component->GetAABB();
-            math::OBB local_obb;
-
-            local_obb.SetFrom(mesh_aabb);
-
-            local_obb.Scale(math::float3::zero, mesh_component->Owner()->Transform()->GetScale());
-            local_obb.Transform(mesh_component->Owner()->Transform()->GetRotation());
-            local_obb.Translate(mesh_component->Owner()->Transform()->GetPosition());
-
-
-            aabb.Enclose(local_obb);
-        }
-        obb.SetFrom(aabb);
-    }
     else
     {
+        // Set a 1 unit cube if there is no mesh component in owner or
+        // its descendants:
         obb.SetFrom(math::AABB(math::float3(-0.5f, -0.5f, -0.5f), math::float3(0.5f, 0.5f, 0.5f)));
         obb.Transform(Owner()->Transform()->GetRotation());
         obb.Translate(Owner()->Transform()->GetPosition());
+        // NOTE: We don't scale obb by the global scale of its parent since
+        // we don't want an empty obb to take more space in the world.
     }
 
     // NOTE: For now this is not a true obb, it's just an obb that acts like aabb,
     // rotations are not truly captured to be more precise. Maybe using child bound
     // ing boxes would give a better result.
-
-    //// Form OBB from temp aabb:
-    //obb.SetFrom(aabb);
 
     // Get Minimal enclosing sphere radius:
     const math::Sphere minimal_enclosing_sphere = obb.MinimalEnclosingSphere();
@@ -170,6 +167,57 @@ math::float3 ComponentBoundingBox::GetCenterPosition()
     return center_position;
 }
 
+void ComponentBoundingBox::DrawInspectorContent()
+{
+    math::float3 obb_half_size = obb.HalfSize();
+    math::float3 obb_position = obb.pos;
+
+    ImGui::TextWrapped("Half Size: %f, %f, %f", obb_half_size.x, obb_half_size.y, obb_half_size.z);
+    ImGui::TextWrapped("Position: %f, %f, %f", obb_position.x, obb_position.y, obb_position.z);
+}
+
+void ComponentBoundingBox::AddMeshComponentToAABB(ComponentMesh* mesh, math::AABB& aabb) const
+{
+    // Get AABB of the mesh:
+    const math::AABB& mesh_aabb = mesh->GetAABB();
+    // Construct a local_obb that will hold the
+    // transformed version of mesh's aabb:
+    math::OBB local_obb;
+
+    // Set local_obb to have mesh_aabb:
+    local_obb.SetFrom(mesh_aabb);
+    // Set its scale:
+    local_obb.Scale(math::float3::zero, mesh->Owner()->Transform()->GetScale());
+    // Set its rotation:
+    local_obb.Transform(mesh->Owner()->Transform()->GetRotation());
+    // Set its position:
+    local_obb.Translate(mesh->Owner()->Transform()->GetPosition());
+
+    // NOTE: These transforms were made separately to have more 
+    // precise transform of the obb as the directly transforming
+    // it with the matrix of mesh's owner's transform can have errors.
+
+    // Enclose the local_obb with the aabb:
+    aabb.Enclose(local_obb);
+}
+
+void ComponentBoundingBox::HandleHierarchyChanged(entity_operation operation)
+{
+    if (operation != entity_operation::CHILDREN_CHANGED)
+    {
+        return;
+    }
+
+    // NOTE: Right now we only care about the changes in the children
+    // entity to recalculate this bounding box. However this is not
+    // the most optimal way to do this, we could have a more specific
+    // event such as HandleChildWithMeshComponent added type of event
+    // but it would be so specific that it could also decrease the
+    // performance. Right now invoking this function each time the
+    // children are changed makes sense.
+
+    Load();
+}
 
 void ComponentBoundingBox::HandleComponentChanged(component_type type)
 {
@@ -179,21 +227,12 @@ void ComponentBoundingBox::HandleComponentChanged(component_type type)
 
     switch (type)
     {
-        case component_type::TRANSFORM:
-        case component_type::MESH:
-            break;
-        default:
-            return;
+    case component_type::TRANSFORM:
+    case component_type::MESH:
+        break;
+    default:
+        return;
     }
 
     Load();
-}
-
-void ComponentBoundingBox::DrawInspectorContent()
-{
-    math::float3 obb_half_size = obb.HalfSize();
-    math::float3 obb_position = obb.pos;
-
-    ImGui::TextWrapped("Half Size: %f, %f, %f", obb_half_size.x, obb_half_size.y, obb_half_size.z);
-    ImGui::TextWrapped("Position: %f, %f, %f", obb_position.x, obb_position.y, obb_position.z);
 }
--- Source/ComponentBoundingBox.h
+++ Source/ComponentBoundingBox.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "Entity.h"
 #include "Component.h"
 #include "Event.h"
-#include <functional>
 
 #include "MATH_GEO_LIB/Geometry/OBB.h"
+
+class ComponentMesh;
 
 class ComponentBoundingBox : public Component
 {
@@ -13,6 +15,7 @@ private:
 	float minimal_enclosing_sphere_radius;
 	math::float3 center_position;
 	EventListener<component_type> component_changed_event_listener;
+	EventListener<entity_operation> hierarchy_changed_event_listener;
 
 public:
 	ComponentBoundingBox();
@@ -21,7 +24,6 @@ public:
 	component_type Type() const override;
 	void Initialize(Entity* new_owner) override;
 	void Load();
-	void HandleComponentChanged(component_type type);
 	void Update() override;
 	void DrawGizmo() override;
 	const math::OBB& GetBoundingBox();
@@ -30,4 +32,9 @@ public:
 
 protected:
 	void DrawInspectorContent() override;
+
+private:
+	void AddMeshComponentToAABB(ComponentMesh* mesh, math::AABB& aabb) const;
+	void HandleHierarchyChanged(entity_operation operation);
+	void HandleComponentChanged(component_type type);
 };
